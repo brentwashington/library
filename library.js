28c7b97071ba70547cd20d1c02e9bf1ca6481760
Brent Washington <brentwashing@gmail.com>
2021-07-23 17:13:49 -0500

    Refactor display into table

--- library.js
+++ library.js
@@ -1,8 +1,6 @@
 let book1 = new Book("S.E. Hinton", "The Outsiders", 192, true);
 let book2 = new Book("J.R.R. Tolkien", "The Hobbit", 304, false);
 
-let addBookButton = document.getElementById("add-book");
-
 let myLibrary = [book1, book2];
 
 // Constructor for Book
@@ -15,7 +13,10 @@ function Book(author, title, pages, read) {
 
 function addBookToLibrary() {
   for (let book of myLibrary) {
-      console.log(book);
+    let para = document.createElement("p");
+    let bookDetails = document.createTextNode(`${book.title}, ${book.author}, ${book.pages} pages`);
+    para.appendChild(bookDetails);
+    document.body.appendChild(para);
   }
 }
 
